f3aac6c937f9cc55fbec0340441f0332ab930bb8
n8nunes <n8nunes05@gmail.com>
2024-04-16 09:14:25 +1000

    added menu description

--- source/MarketBuy.py
+++ source/MarketBuy.py
@@ -1,16 +1,19 @@
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
-import Action
-import User
+from Action import Action
+from User import User
 class MarketBuy(Action):
     def execute(self, user: User) -> str: 
         symbol = input("Enter the ticker symbol: ").capitalize()
-        qty = float(input("Enter the quantity you wish to trade to 3 decimal points: "))
+        qty = float(input("Enter the quantity you wish to trade to 3 decimal places: "))
         side = OrderSide.BUY
         time_in_force = TimeInForce.DAY
-        trading_client = user().get_trading_client()
+        trading_client = user.get_trading_client()
 
         market_order_data = MarketOrderRequest(symbol, qty, side, time_in_force)
-        market_order = trading_client
-
+        market_order = trading_client.submit_order(market_order_data)
+        return "Order placed!"
+    
+    def menudescription(self) -> str:
+        return "Buy a stock at the current market price."
