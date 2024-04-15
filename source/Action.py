3f6ec05a2142df605f69cb55f6364258b8acac26
n8nunes <n8nunes05@gmail.com>
2024-04-15 22:29:01 +1000

    implemented template for all actions

---
+++ source/Action.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class Action(ABC):
+    @abstractmethod
+    def execute (self) -> str:
+        pass
+
+    @abstractmethod
+    def menudescription (self) -> str:
+        pass
