4165a6b1829735b3d2135131f0d3cc375ec37a67
n8nunes <n8nunes05@gmail.com>
2024-04-16 09:23:32 +1000

    syntax fix

--- source/MarketBuy.py
+++ source/MarketBuy.py
@@ -6,12 +6,17 @@ from User import User
 class MarketBuy(Action):
     def execute(self, user: User) -> str: 
         symbol = input("Enter the ticker symbol: ").capitalize()
-        qty = float(input("Enter the quantity you wish to trade to 3 decimal places: "))
+        qty = float(input("Enter the quantity you wish to buy to 3 decimal places: "))
         side = OrderSide.BUY
         time_in_force = TimeInForce.DAY
         trading_client = user.get_trading_client()
 
-        market_order_data = MarketOrderRequest(symbol, qty, side, time_in_force)
+        market_order_data = MarketOrderRequest(
+            symbol=symbol, 
+            qty=qty, 
+            side=side, 
+            time_in_force=time_in_force
+            )
         market_order = trading_client.submit_order(market_order_data)
         return "Order placed!"
     
