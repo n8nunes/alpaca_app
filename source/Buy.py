2fbc1a8857de4a7812fb82cbe2e1a6b5690ced2d
n8nunes <n8nunes05@gmail.com>
2024-04-18 12:43:17 +1000

    created an abstract Buy class to reduce repetition

---
+++ source/Buy.py
@@ -0,0 +1,25 @@
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
+from Action import Action
+from User import User
+from abc import ABC, abstractmethod
+
+class Buy(Action, ABC):
+    def __init__(self, user: User, symbol: str, qty: float, time_in_force: TimeInForce):
+        self.user = user
+        self.symbol = symbol
+        self.qty = qty
+        self.time_in_force = time_in_force
+        self.side = OrderSide.BUY
+        self.trading_client = self.user.get_trading_client()
+    
+    @abstractmethod
+    def execute(self, user: User) -> str: 
+        # create an order object
+        # pass the order object into trading_client.submit_order()
+        pass
+    
+    @abstractmethod
+    def menudescription(self) -> str:
+        pass
